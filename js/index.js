59fac1220ef94ebc416f005846b1c8f8a55ae742
Ayaz <ayazzlockhat@gmail.com>
2021-10-06 00:57:31 -0400

    Added mobile screen scaling.

--- js/index.js
+++ js/index.js
@@ -9,6 +9,30 @@ window.onload = function() {
       } else {
         document.getElementById("navbarscroll").style.top = "-60px";
       }
+
+      if (prevScrollpos > currentScrollPos) {
+        document.getElementById("sidebarscroll").style.top = "0";
+      } else {
+        document.getElementById("sidebarscroll").style.top = "-200px";
+      }
+
+      if (prevScrollpos > currentScrollPos) {
+        document.getElementById("mySidebar").style.top = "0";
+      } else {
+        document.getElementById("mySidebar").style.top = "-275px";
+      }
       prevScrollpos = currentScrollPos;
     }
 };
+
+    // Mobile SideBar
+    function openNav() {
+      document.getElementById("mySidebar").style.width = "250px";
+      document.getElementById("main").style.marginRight = "250px";
+    }
+    
+    /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+    function closeNav() {
+      document.getElementById("mySidebar").style.width = "0";
+      document.getElementById("main").style.marginRight = "0";
+    }

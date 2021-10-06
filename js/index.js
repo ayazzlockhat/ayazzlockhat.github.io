ea37aa7dc477eb471cd1f7d5d4e18f550e239b94
Ayaz <ayazzlockhat@gmail.com>
2021-10-06 14:10:31 -0400

    open button moves away after clicking for cleaner apperance

--- js/index.js
+++ js/index.js
@@ -28,11 +28,10 @@ window.onload = function() {
     // Mobile SideBar
     function openNav() {
       document.getElementById("mySidebar").style.width = "250px";
-      document.getElementById("main").style.marginRight = "250px";
+      document.getElementById("main").style.marginLeft = "250px";
     }
     
-    /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
     function closeNav() {
       document.getElementById("mySidebar").style.width = "0";
-      document.getElementById("main").style.marginRight = "0";
+      document.getElementById("main").style.marginLeft = "0";
     }
